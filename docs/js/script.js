9dc767ef42356db0bd420a77ec736e6a8a11de3a
unknown <marat76593@gmail.com>
2025-04-22 15:17:19 +0300

    deploy: add folder docs from source

---
+++ docs/js/script.js
@@ -0,0 +1,49 @@
+'strict mode';
+
+let headerWrapper = document.querySelector('.page-header__wrapper');
+let menu = document.querySelector('.page-header__toggle');
+let navList = document.querySelector('.main-nav__list');
+
+navList.classList.remove('main-nav__list--nojs');
+navList.classList.remove('main-nav__list--opened');
+headerWrapper.classList.remove('page-header__wrapper--nojs');
+
+menu.addEventListener('click', function() {
+    navList.classList.toggle('main-nav__list--opened');
+    menu.classList.toggle('page-header__toggle--closed');
+})
+
+
+let buttonPopup = document.querySelectorAll('.button--popup');
+let popupForm = document.querySelector('.popup-form__form');
+let success = document.querySelector('.popup-success');
+let popupClose = document.querySelectorAll('.popup-form__close');
+
+buttonPopup.forEach((item) =>  {
+    item.addEventListener('click', () => {
+        popupForm.style.display = 'block';
+    })
+});
+
+popupClose.forEach((item) => {
+    item.addEventListener('click', () => {
+        popupForm.style.display = 'none';
+        success.style.display = 'none';
+    })
+})
+
+let onFormEscKeydown = (evt) => {
+    if (evt.key === 'Escape' || evt.key === 'Esc') {
+        evt.preventDefault();
+        popupForm.style.display = 'none';
+        success.style.display = 'none';
+    }
+};
+
+document.addEventListener('keydown', onFormEscKeydown);
+
+popupForm.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    popupForm.style.display = 'none';
+    success.style.display = 'flex';
+})
